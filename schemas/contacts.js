75059278d7c466a172779c9c04027ebaf350ef48
nicksolony <nicksolony@gmail.com>
2023-12-01 15:36:49 -0500

    added  mongoose schema

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -6,8 +6,26 @@ const contactsJoiSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     phone: myCustomJoi.string().phoneNumber().required(),
-})
+});
+
+const contactsMongooseSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    }
+});
 
 module.exports = {
     contactsJoiSchema,
+    contactsMongooseSchema
 }

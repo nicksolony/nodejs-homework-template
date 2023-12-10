671d228b787a870adc8cfa2558e2fb143d65a3fb
nicksolony <nicksolony@gmail.com>
2023-12-10 11:24:13 -0500

    fixed logout process

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -15,8 +15,6 @@ const authenticate = async (req, res, next) => {
     try {
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
-        console.log(user.token);
-        console.log(token);
         if(!user || !user.token || user.token !== token) {
             next(HttpError(401)); 
         }

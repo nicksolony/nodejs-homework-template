7c6a04f712b0ade852a4b9dc3a86010e40263873
nicksolony <nicksolony@gmail.com>
2023-12-11 17:37:49 -0500

    added authentication to contacts router

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,22 +2,22 @@ const express = require('express');
 
 const contactsController = require('../../controllers/contacts');
 
-const { validateBody, isValidId } = require('../../middlewares');
+const { validateBody, isValidId, authenticate } = require('../../middlewares');
 
 const {schemas} = require('../../models/contact');
 
 const router = express.Router()
 
-router.get('/', contactsController.getAll);
+router.get('/',authenticate, contactsController.getAll);
 
-router.get('/:contactId', isValidId, contactsController.getById);
+router.get('/:contactId',authenticate, isValidId, contactsController.getById);
 
-router.post('/', validateBody(schemas.contactsJoiSchema), contactsController.add);
+router.post('/',authenticate, validateBody(schemas.contactsJoiSchema), contactsController.add);
 
-router.delete('/:contactId', isValidId, contactsController.deleteById);
+router.delete('/:contactId',authenticate, isValidId, contactsController.deleteById);
 
-router.put('/:contactId', isValidId, validateBody(schemas.contactsJoiSchema), contactsController.updateById);
+router.put('/:contactId',authenticate, isValidId, validateBody(schemas.contactsJoiSchema), contactsController.updateById);
 
-router.patch("/:contactId/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), contactsController.updateStatusContact);
+router.patch("/:contactId/favorite",authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), contactsController.updateStatusContact);
 
 module.exports = router;
